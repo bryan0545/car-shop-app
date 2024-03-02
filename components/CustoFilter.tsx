e4023900a52beffc326709add756843974cddd0a
Rodas <brayan-rodas@hotmail.com>
2024-03-02 12:23:53 -0500

    add search components and footer

---
+++ components/CustoFilter.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustoFilter = () => {
+  return (
+    <div>CustoFilter</div>
+  )
+}
+
+export default CustoFilter
--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,16 @@ import Hero from './Hero';
 import CustomButton from './CustomButton';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import CustoFilter from './CustoFilter';
+import SearchBar from './SearchBar';
+import SearchManufacturer from './SearchManufacturer';
 
 export {
     Hero,
     CustomButton,
     Navbar,
-    Footer
+    Footer,
+    CustoFilter,
+    SearchManufacturer,
+    SearchBar
 }
